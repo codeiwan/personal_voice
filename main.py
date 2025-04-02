da010f6a074cdeb3d342adfe305ea58b3272e3e8
codeiwan <pewlv7976@gmail.com>
2025-04-02 17:13:26 +0900

    test용 api 작성

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from Render + FastAPI!"}
+
+
+@app.get("/test_get/echo-number")
+def echo_number(number: int = Query(..., description="숫자를 쿼리 파라미터로 전달하세요.")):
+    return {
+        "received_number": number,
+        "message": "백엔드 GET test API가 제공한 숫자입니다."
+    }
+
+class NumberInput(BaseModel):
+    number: int
+
+@app.post("/test_post/echo-number")
+def echo_number(data: NumberInput):
+    return {
+        "received_number": data.number,
+        "message": "백엔드 POST test API가 제공한 숫자입니다."
+    }
